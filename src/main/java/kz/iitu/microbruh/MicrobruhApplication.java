b652af0230ffb07821a3f6eefd27fb4d1432d70a
605-11 <kboichenko06@gmail.com>
2024-11-19 12:11:28 +0500

    init commit

---
+++ src/main/java/kz/iitu/microbruh/MicrobruhApplication.java
@@ -0,0 +1,13 @@
+package kz.iitu.microbruh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrobruhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrobruhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/iitu/microbruh/MicrobruhApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.iitu.microbruh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrobruhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
